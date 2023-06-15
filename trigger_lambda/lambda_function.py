5dad6c0c5236824d520ab095a543cb4987ee27ea
George Davis <george@triumphtech.com>
2023-06-15 15:56:30 -0700

    feat: Adding context specifically AWS Request ID to know who is invoking the Lambda function and for searchabability on CloudWatch Log Insights later

--- trigger_lambda/lambda_function.py
+++ trigger_lambda/lambda_function.py
@@ -7,10 +7,12 @@ import time
 # Trigger a lambda function from within this Lambda function
 def trigger_invocation_on_docdb_reader_lambda(function_name, invocation_type):
 
-    lambda_client.invoke(
+    lambdaInvokeResponse = lambda_client.invoke(
         FunctionName = function_name,
         InvocationType = invocation_type,
     )
+
+    logger.info("Lambda Invoke Response: {}".format(lambdaInvokeResponse))
 
 def send_sns_alert(message):
     """send an SNS alert"""
@@ -38,11 +40,12 @@ def lambda_handler(event, context):
     """Trigger a given Lambda function in short intervals for that Lambda function to typically compute. This is a workaround for Events Rule which cannot do trigger less than a minute"""
 
     logger.debug("Event: {}".format(event))
+    logger.debug("Context: {}".format(context))    
 
     lambda_function_name = str(os.environ.get("LAMBDA_FUNCTION_NAME"))
-    trigger_lambda_timeout = os.environ.get("TRIGGER_LAMBDA_TIMEOUT")
+    trigger_lambda_timeout = int(os.environ.get("TRIGGER_LAMBDA_TIMEOUT"))
     invocation_type = str(os.environ.get("INVOCATION_TYPE"))
-    invocation_time_interval = str(os.environ.get("INVOCATION_TIME_INTERVAL"))
+    invocation_time_interval = int(os.environ.get("INVOCATION_TIME_INTERVAL"))
 
     events_processed = 0
     is_error = 0
@@ -53,27 +56,27 @@ def lambda_handler(event, context):
         # Runs every second until the Lambda function times out. You could set a larger lambda timeout (max 15 minutes).
         iter = 0 
         while iter < (trigger_lambda_timeout / invocation_time_interval):
-            logger.info("Invoking {}...".format(lambda_function_name))
+            logger.info("Invoking {} using AWS Request ID: {}...".format(lambda_function_name, context.aws_request_id))
             trigger_invocation_on_docdb_reader_lambda(lambda_function_name, invocation_type)
             events_processed += 1
-            time.sleep(secs=invocation_time_interval)
+            time.sleep(invocation_time_interval)
             iter += 1
 
     except Exception as ex:
-        logger.error('Exception in invoking {} : {}'.format(lambda_function_name, ex))
+        logger.error('Exception in invoking {} using AWS Request ID: {} : {}'.format(lambda_function_name, context.aws_request_id, ex))
         is_error = 1
         send_sns_alert(str(ex))
         raise
 
     finally:
-        logger.info("{} Invocations Complete!".format(events_processed))
+        logger.info("{} Invocations Complete using AWS Request ID: {}".format(events_processed, context.aws_request_id))
 
         return {
                 'statusCode': success_status_code_by_invocation_type[invocation_type],
                 'description': 'Success',
-                'detail': json.dumps(str(events_processed)+ ' records processed successfully.')
+                'detail': '{} records processed successfully using AWS Request ID: {}'.format(events_processed, context.aws_request_id)
             } if is_error == 0 else {
                 'statusCode': 0,
                 'description': 'Failure',
-                'detail': '{} records processed successfully.'.format(events_processed)
+                'detail': 'Failed but {} records were processed successfully using AWS Request ID: {}.'.format(events_processed, context.aws_request_id)
             }
